270a33202da0d61a3c3cf5249397a0eb2e1965d8
Muhamedli <alexkouzenkov@gmail.com>
2024-08-28 23:05:44 +0300

    return velocity from axis

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import roboticstoolbox as rtb
 import spatialmath as sm
 import spatialgeometry as sg
 import swift
+import numpy as np
 
 env = swift.Swift()
 env.launch(realtime=True)
@@ -20,14 +21,22 @@ for i in traj1.q:  # for each joint configuration on trajectory
     robot.q = i  # update the robot state
     env.step(0.01)  # update visualization
 
-
-
-Tep2 = robot.fkine(robot.q) * sm.SE3.Trans(0.2, 0, 0) * sm.SE3.OA([0, 1, 0], [0, 0, 1])
-goal_ax = sg.Axes(0.1, pose=Tep2)
+Tep2 = robot.fkine(robot.q) * sm.SE3.Trans(0.2, 0, 0) * sm.SE3.OA([0, 1, 0], [0, 0, 1])  # Задание целевой позы
+goal_ax = sg.Axes(0.1, pose=Tep2)  # построение системы координат в ней
 env.add(goal_ax)
-traj2 = rtb.tools.trajectory.ctraj(Tep1, Tep2, 25)
-cartesian_sol = robot.ikine_LM(traj2)
-# print(cartesian_sol.q)
+step = 25  # кол-во точек разбиения траектории
+time = 1  # время прохождения траектории
+time_point_interval = time / step
+traj2 = rtb.tools.trajectory.ctraj(Tep1, Tep2, step)  # построение траектории
+cartesian_sol = robot.ikine_LM(traj2)  # получение обобщенных координат каждой точки траектории
+
+velocity_array = np.empty((step, 6))
+for i in range(step - 1):
+    for k in range(6):
+        velocity_array[i][k] = (cartesian_sol.q[i + 1][k] - cartesian_sol.q[i][
+            k]) / time_point_interval  # вычисление обобщенных скоростей в осях для каждой точки траектории
+np.set_printoptions(precision=3, linewidth=100, suppress=True)
+print(velocity_array)
 
 for i in cartesian_sol.q:
     # goal_ax = sg.Axes(0.1, pose=robot.fkine(i))
