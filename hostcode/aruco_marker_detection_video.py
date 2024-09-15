6589ca71adc695a9a2b167a6ec79afde1f90f2dd
Muhamedli <alexkouzenkov@gmail.com>
2024-09-16 01:21:00 +0300

    ctraj - add graphics velocity in each axis

--- hostcode/aruco_marker_detection_video.py
+++ hostcode/aruco_marker_detection_video.py
@@ -8,38 +8,38 @@ while True:
 
     # Define ArUco detection parameters
     arucoParams = cv2.aruco.DetectorParameters()
-    arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+    arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
 
     # Perform ArUco marker detection
     detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
     (corners, ids, rejected) = detector.detectMarkers(img)
     # print(corners)
-    if len(corners)!= 0:
+    if len(corners) != 0:
         # Display corners of marker
         # cv2.aruco.drawDetectedMarkers(img, corners, ids)
         cv2.aruco.drawDetectedMarkers(img, corners)
-  
+
         # Display center of marker
         x_sum = corners[0][0][0][0] + corners[0][0][1][0] + corners[0][0][2][0] + corners[0][0][3][0]
         y_sum = corners[0][0][0][1] + corners[0][0][1][1] + corners[0][0][2][1] + corners[0][0][3][1]
         x_centerPixel = x_sum * .25
         y_centerPixel = y_sum * .25
         print("x: ", x_centerPixel, "y: ", y_centerPixel)
-        cv2.circle(img,(int(x_centerPixel), int(y_centerPixel)), radius=int((corners[0][0][1][0] - corners[0][0][0][0]) * 0.05), color=(0, 0, 255), thickness=-1)
+        cv2.circle(img, (int(x_centerPixel), int(y_centerPixel)),
+                   radius=int((corners[0][0][1][0] - corners[0][0][0][0]) * 0.05), color=(0, 0, 255), thickness=1)
 
         # Display size of the image
         # print("width: ", img.shape[1], "height: ", img.shape[0])
         # Display size of the marker
         # print(corners[0][0][1][0] - corners[0][0][0][0])
-
         # Display angles of joints
         font = cv2.FONT_HERSHEY_COMPLEX
-        bottomLeftCornerOfText = (img.shape[1]//8, img.shape[0]//10)
+        bottomLeftCornerOfText = (img.shape[1] // 8, img.shape[0] // 10)
         fontScale = 1
-        fontColor = (0, 0, 0)
-        thickness = 1
-        lineType = 2
-    
+        fontColor = (147, 20, 255)
+        thickness = 2
+        lineType = 1
+
         angle_joint_1 = 0
         angle_joint_2 = 0
         angle_joint_3 = 0
@@ -47,16 +47,16 @@ while True:
         angle_joint_5 = 0
         angle_joint_6 = 0
 
-        text = (f"Угол поворота первого звена: {angle_joint_1} \n"
-                f"Угол поворота второго звена: {angle_joint_2}\n"
-                f"Угол поворота третьего звена: {angle_joint_3}\n"
-                f"Угол поворота четвертого звена: {angle_joint_4}\n"
-                f"Угол поворота пятого звена: {angle_joint_5}\n"
-                f"Угол поворота шестого звена: {angle_joint_6}\n"
+        text = (f"angle joint_1: {angle_joint_1} \n"
+                f"angle joint_2: {angle_joint_2}\n"
+                f"angle joint_3: {angle_joint_3}\n"
+                f"angle joint_4: {angle_joint_4}\n"
+                f"angle joint_5: {angle_joint_5}\n"
+                f"angle joint_6: {angle_joint_6}\n"
                 )
         y0, dy = 50, 50
         for i, line in enumerate(text.split('\n')):
-            y = y0 + i*dy
+            y = y0 + i * dy
             cv2.putText(img, line, (50, y),
                         font,
                         fontScale,
@@ -64,8 +64,9 @@ while True:
                         thickness,
                         lineType)
     # Display image
-    cv2.imshow("Image", img)
+    cv2.namedWindow("Object_detection", cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow("Object_detection", width=1248, height=702)
+    cv2.imshow("Object_detection", img)
     # Wait for the key press
     if cv2.waitKey(1) == ord('q'):
         break
-  
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import time
+
 import roboticstoolbox as rtb
 import spatialmath as sm
 import spatialgeometry as sg
@@ -21,33 +23,13 @@ for i in traj1.q:  # for each joint configuration on trajectory
     robot.q = i  # update the robot state
     env.step(0.01)  # update visualization
 
-Tep2 = robot.fkine(robot.q) * sm.SE3.Trans(0.2, 0, 0) * sm.SE3.OA([0, 1, 0], [0, 0, 1])  # Задание целевой позы
-goal_ax = sg.Axes(0.1, pose=Tep2)  # построение системы координат в ней
-env.add(goal_ax)
-time_point_interval = 0.02  # кол-во точек разбиения траектории
-time = 1  # время прохождения траектории
-step = int(time / time_point_interval)
-traj2 = rtb.tools.trajectory.ctraj(Tep1, Tep2, step)  # построение траектории
-cartesian_sol = robot.ikine_LM(traj2)  # получение обобщенных координат каждой точки траектории
-# print(cartesian_sol.q)
-
-velocity_array = np.empty((step, 6))
-for i in range(step - 1):
-    for k in range(6):
-        velocity_array[i][k] = (cartesian_sol.q[i + 1][k] - cartesian_sol.q[i][
-            k]) / time_point_interval  # вычисление обобщенных скоростей в осях для каждой точки траектории
-# np.set_printoptions(linewidth=100, suppress=True)
-# print(velocity_array)
-
-# for i in cartesian_sol.q: # Отображение позы перебором обобщенных координат
-#     # goal_ax = sg.Axes(0.1, pose=robot.fkine(i)) # Построение траектории
-#     # env.add(goal_ax)
-#     robot.q = i
-#     env.step(time_point_interval)
-
-for z in range(step - 1):  # Отображение позы заданием скоростей
-    robot.qd = velocity_array[z]
-    # link_ax = sg.Axes(0.1, pose=robot.fkine(robot.q))  # Построение траектории
-    # env.add(link_ax)
-    z += 1
-    env.step(time_point_interval)
+gripper = robot.grippers[0]
+for i in range(0, 38):  # Moving gripper
+    # print(i)
+    gripper.q = [i / 1000]
+    env.step(0.01)
+time.sleep(3)
+for i in range(0, 38):  # Moving gripper
+    # print(i)
+    gripper.q = [(37 - i) / 1000]
+    env.step(0.01)
