27e0266468b67bf1621e228b2c30f5878f37e781
Slava Napalkov <napalkov2005@gmail.com>
2024-09-19 21:27:23 +0300

    stability has been improver; added running avarage adopted filter

--- hostcode/taking.py
+++ hostcode/taking.py
@@ -16,16 +16,44 @@ env.add(robot)
 def TrajToSurf(x, y):
     Tep = robot.fkine(robot.qr) * sm.SE3.Trans(x, hight, y)
     sol = robot.ik_LM(Tep)
-
     traj = rtb.tools.trajectory.jtraj(robot.qz, sol[0], 100)
     return traj, Tep
 
+def chekTaskPresessing(tvec, sol):
+    tvec_env = [0,0,0]
+    tvec_cam_l = 0
+    tvec_env_l = 0
+
+    print((robot.fkine(sol).A)[2][3],(robot.fkine(robot.qr).A)[2][3])
+
+    #СЛЕДИТЬ ЗА НАПРАВЛЕНИЕМ ОСЕЙ БЛЕД!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    for i in range(3):
+        if i==0:
+            tvec_env[i] =  ((robot.fkine(sol).A)[i][3]-(robot.fkine(robot.qr).A)[i][3])
+        else:
+            tvec_env[i] =  ((robot.fkine(sol).A)[i][3]-(robot.fkine(robot.qr).A)[i][3])*-1
+        # tvec_env_l += tvec_env[i]**2
+        # tvec_cam_l += tvec[i]**2
+
+    #print(tvec_env, tvec)
+
+    if (abs(tvec[2] - tvec_env[2]) + abs(tvec[0] - tvec_env[0]) + abs(tvec[1] - tvec_env[1])< 0.02):
+        return 1
+    else:
+        return 0
+
 
 def SolDim(tvec):
-    Tep = robot.fkine(robot.qr) * sm.SE3.Trans(tvec[0], tvec[2], tvec[1])
+    Tep = robot.fkine(robot.qr) * sm.SE3.Trans(tvec[0], tvec[1], tvec[2])
     sol = robot.ik_LM(Tep)
-    return sol[0]
-
+    if(chekTaskPresessing(tvec, sol[0])):
+    #if(1):
+        return sol[0]
+    else:
+        #print("InvTask solved incorrectly")
+        return robot.q
+        
 
 def SolDimArray(tvec):
     # sol = SolDim(list(map(lambda x: round(x, ndigits = 4),tvec)))
