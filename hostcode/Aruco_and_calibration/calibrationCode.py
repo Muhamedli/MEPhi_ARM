b548f55778b7097190310ff379adc6f558d95852
Muhamedli <alexkouzenkov@gmail.com>
2024-09-20 19:32:40 +0300

    charuco calibration

--- hostcode/Aruco_and_calibration/calibrationCode.py
+++ hostcode/Aruco_and_calibration/calibrationCode.py
@@ -27,63 +27,60 @@ objectp3d[0, :, :2] = np.mgrid[0:CHECKERBOARD[0],
                       0:CHECKERBOARD[1]].T.reshape(-1, 2)
 prev_img_shape = None
 
-images = glob.glob('C:/coding/git/Robot-manipulator/hostcode/Aruco_and_calibration/samples/*.jpg')
+images = glob.glob('samples/*.jpg')
 
-for filename in images: 
-    image = cv2.imread(filename) 
+for filename in images:
+    image = cv2.imread(filename)
     grayColor = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-
-    # Find the chess board corners 
+    # Find the chess board corners
     # If desired number of corners are 
     # found in the image then ret = true 
-    ret, corners = cv2.findChessboardCorners( 
-                    grayColor, CHECKERBOARD,  
-                    cv2.CALIB_CB_ADAPTIVE_THRESH  
-                    + cv2.CALIB_CB_FAST_CHECK +                     cv2.CALIB_CB_NORMALIZE_IMAGE) 
+    ret, corners = cv2.findChessboardCorners(
+        grayColor, CHECKERBOARD,
+        cv2.CALIB_CB_ADAPTIVE_THRESH
+        + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
 
     # If desired number of corners can be detected then, 
     # refine the pixel coordinates and display 
     # them on the images of checker board 
-    if ret == True: 
-        threedpoints.append(objectp3d) 
+    if ret == True:
+        threedpoints.append(objectp3d)
 
         # Refining pixel coordinates 
         # for given 2d points. 
-        corners2 = cv2.cornerSubPix( 
-            grayColor, corners, (11, 11), (-1, -1), criteria) 
+        corners2 = cv2.cornerSubPix(
+            grayColor, corners, (11, 11), (-1, -1), criteria)
 
-        twodpoints.append(corners2) 
+        twodpoints.append(corners2)
 
         # Draw and display the corners 
-        image = cv2.drawChessboardCorners(image,  
-                                            CHECKERBOARD,  
-                                            corners2, ret) 
+        image = cv2.drawChessboardCorners(image,
+                                          CHECKERBOARD,
+                                          corners2, ret)
 
-  
-# Perform camera calibration by 
+    # Perform camera calibration by
 # passing the value of above found out 3D points (threedpoints) 
 # and its corresponding pixel coordinates of the 
 # detected corners (twodpoints) 
-ret, matrix, distortion, r_vecs, t_vecs = cv2.calibrateCamera( 
-    threedpoints, twodpoints, grayColor.shape[::-1], None, None) 
-  
-  
+ret, matrix, distortion, r_vecs, t_vecs = cv2.calibrateCamera(
+    threedpoints, twodpoints, grayColor.shape[::-1], None, None)
+
 # Displaying required output 
-print(" Camera matrix:") 
-print(matrix) 
-  
-print("\n Distortion coefficient:") 
-print(distortion) 
-  
-print("\n Rotation Vectors:") 
-print(r_vecs) 
-  
-print("\n Translation Vectors:") 
-print(t_vecs) 
+print(" Camera matrix:")
+print(matrix)
+
+print("\n Distortion coefficient:")
+print(distortion)
+
+print("\n Rotation Vectors:")
+print(r_vecs)
+
+print("\n Translation Vectors:")
+print(t_vecs)
 
 data = {"camera_matrix": matrix.tolist(), "dist_coeff": distortion.tolist()}
-fname = "C:/coding/git/Robot-manipulator/hostcode/Aruco_and_calibration/data.json"
+fname = "data.json"
 
 with open(fname, "w") as f:
     json.dump(data, f)
