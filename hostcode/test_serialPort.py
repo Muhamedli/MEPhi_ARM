cb12a1d0d9ef98e8e5014ab6ee2c739a526462db
Slava Napalkov <napalkov2005@gmail.com>
2024-09-26 01:41:57 +0300

    serialPort connection rewrited

--- hostcode/test_serialPort.py
+++ hostcode/test_serialPort.py
@@ -1,25 +1,28 @@
 import serial.tools.list_ports
 import time
 
-def sendValues(deg, speed):
+def serialBegin(port = 5, baytrate = 115200):
+    global ser
+    port = f"COM{port}"  # Replace with the appropriate COM port name
+    ser = serial.Serial(port, baytrate)
+
+def serialSend(deg, speed):
+    global ser
     deg.extend(speed)
-    output_text = ""
-    for i in deg:
-        output_text += str(i) + "/"
+    output_text = "/".join(str(i) for i in deg)
 
-    #ser.write(output_text.encode('raw_unicode_escape'))
-
+    ser.write(output_text.encode('raw_unicode_escape'))
     print(output_text)
 
-
-# port = "COM5"  # Replace with the appropriate COM port name
-# baudrate = 115200  # Replace with the desired baud rate
-#
-# ser = serial.Serial(port, baudrate=baudrate)
+def serialRead():
+    global ser
+    flag = 0
+    while(not flag):
+        flag = ser.read()
 
 deg = [-3.1, 2.4, 8.3, 5.3, 3.9, 0.0]
 speed = [10.0] * 6
 
-sendValues(deg, speed)
+serialSend(deg, speed)
 
 time.sleep(5)
