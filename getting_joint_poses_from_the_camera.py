b3883512b2fda53e91e0e48a366146b2972ca1e2
Muhamedli <alexkouzenkov@gmail.com>
2024-10-18 01:17:47 +0300

    A program for calculating the trajectory to the cube

---
+++ getting_joint_poses_from_the_camera.py
@@ -0,0 +1,129 @@
+import cv2
+from traj_planning import *
+import json
+import keyboard
+
+f = open("Aruco_and_calibration/charuco_board_calibration.json")
+data = json.load(f)
+
+imageWidth = 1920
+imageHight = 1080
+cameraID = 0
+
+undistortAtBeginning = False
+
+camera_matrix = np.array(data["camera_matrix"])
+dist_coefs = np.array(data["dist_coeff"])
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coefs, (imageWidth, imageHight), 1,
+                                                  (imageWidth, imageHight))
+
+flag = 1
+fPressed = 0
+text = ""
+y0, dy = 50, 50
+
+font = cv2.FONT_HERSHEY_COMPLEX
+fontScale = 1
+fontColor = (147, 20, 255)
+thickness = 2
+lineType = 1
+
+camera = cv2.VideoCapture(cameraID, cv2.CAP_DSHOW)
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, imageWidth + 10)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, imageHight + 10)
+
+tvecArray = []
+filValTvec = [0, 0, 0]
+filValRvec = [0, 0, 0]
+
+
+def RunningAverageAdaptive(newVal, filVal):
+    if (abs(newVal - filVal) > 1.5):
+        k = 1.5
+    else:
+        k = 0.2
+
+    filVal += (newVal - filVal) * k
+    return filVal
+
+
+while True:
+    good, img = camera.read()
+
+    if (undistortAtBeginning):
+        img = cv2.undistort(img, camera_matrix, dist_coefs, None, newcameramtx)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgToProduse = gray
+
+    arucoParams = cv2.aruco.DetectorParameters()
+    arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
+    detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
+    (corners, ids, rejected) = detector.detectMarkers(imgToProduse)
+
+    if ids is not None:
+        try:
+            rvec, tvec, _objPoints = cv2.aruco.estimatePoseSingleMarkers(corners, 0.025, camera_matrix, dist_coefs)
+            cv2.drawFrameAxes(img, camera_matrix, dist_coefs, rvec, tvec, length=0.025)
+            for i in range(len(tvec[0][0])):
+                filValTvec[i] = RunningAverageAdaptive(tvec[0][0][i], filValTvec[i])
+                # filValRvec[i] = RunningAverageAdaptive(rvec[0][0][i], filValRvec[i])
+
+            tvec[0][0] = filValTvec
+            # rvec[0][0] = filValRvec
+
+            tvec[0][0][0] -= 0.005
+            tvec[0][0][1] -= 0.03
+            tvec[0][0][2] -= (0.0562 + 0.05)
+
+            trans_matrix = cv2.Rodrigues(rvec)
+
+            trans_matrix[0][1] = [-i for i in np.array(trans_matrix[0])[1]]
+            trans_matrix[0][2] = [-i for i in np.array(trans_matrix[0])[2]]
+
+            if (flag):
+                angle_joint = SolDegrees(tvec[0][0], trans_matrix[0], const_orient=True)
+                robot.q = angle_joint
+                travel_time = 5  # время прохода траектории
+                steps = 50  # Число точек разбиения (кол-во пакетов углов в traj_for_AI.q)
+                time_step = travel_time / steps
+                time_vec = np.arange(0, travel_time, time_step)
+                traj_for_AI = rtb.tools.trajectory.jtraj(robot.qr, robot.q, time_vec)
+                # print(angle_joint) # Углы осей в целевой точке
+                # print(traj_for_AI.q) # Углы осей при движении
+
+            text = ""
+            for i in range(len(angle_joint)):
+                text += f"angle joint {i + 1}: {angle_joint[i]}\n"
+
+        except:
+            print("trouble")
+
+        cv2.aruco.drawDetectedMarkers(img, corners)
+
+    # Display image
+    for i, line in enumerate(text.split('\n')):
+        y = y0 + i * dy
+        cv2.putText(img, line, (50, y),
+                    font,
+                    fontScale,
+                    fontColor,
+                    thickness,
+                    lineType)
+
+    if (undistortAtBeginning):
+        img = cv2.undistort(img, camera_matrix, dist_coefs, None, newcameramtx)
+
+    cv2.namedWindow("Object_detection", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Object_detection", width=imageWidth, height=imageHight)
+    cv2.imshow("Object_detection", img)
+    cv2.waitKey(1)
+
+    if keyboard.is_pressed('f'):
+        if fPressed == 0:
+            flag = (flag + 1) % 2
+            fPressed = 1
+        elif fPressed == 1:
+            fPressed = 0
+    if keyboard.is_pressed('q'):  # if key 'q' is pressed
+        break
---
+++ traj_planning.py
@@ -0,0 +1,98 @@
+import roboticstoolbox as rtb
+import spatialmath as sm
+import numpy as np
+import swift
+
+hight = 0.308
+
+robot = rtb.models.MEPhI_ARM()
+robot.q = robot.qz
+
+env = swift.Swift()
+env.launch(realtime=True)
+env.add(robot)
+
+
+def SolDegrees(tvec, trans_matrix, q=robot.qr, const_orient=True):
+    sol = SolFinder(tvec, trans_matrix, const_orient, q)
+    # robot.q = sol
+    env.step()
+    sol = list(map(lambda x: round((x / np.pi * 180), ndigits=1), sol))
+    return sol
+
+
+# def TrajFromQToPoint(tvec, trans_matrix, q=robot.qr, const_orient=True):
+#     sol = SolFinder(tvec, trans_matrix, const_orient, q)
+#     traj = rtb.tools.trajectory.jtraj(q, sol, 10)
+#     return traj
+
+
+def SolFinder(tvec, trans_matrix, const_orient=True, q=robot.qr):
+    '''поиск и верефикация решения в конечной точке'''
+
+    new_y_axis_orientation = trans_matrix[1]
+
+    if (const_orient):
+        # new_y_axis_orientation[1] = 1 #Возможно здесь нужно будет потом заменить на ноль
+        new_y_axis_orientation[2] = 0
+        new_z_axis_orientation = [0, 0, 1]
+    else:
+        new_z_axis_orientation = trans_matrix[2]
+
+    Tep = robot.fkine(q) * sm.SE3.Trans(tvec[0], tvec[1], tvec[2]) * sm.SE3.OA(new_y_axis_orientation,
+                                                                               new_z_axis_orientation)
+    sol = robot.ik_LM(Tep)
+
+    if (chekTaskPresessing(tvec, sol[0], q)):
+        return sol[0]
+    else:
+        print("trouble")
+        return robot.q
+
+
+def chekTaskPresessing(tvec, sol, q):
+    '''функция для сопоставления найденного решения и входных данных'''
+
+    tvec_env, rvec_env = FromQtoVec(q, sol)
+    if (abs(tvec[2] - tvec_env[2]) + abs(tvec[0] - tvec_env[0]) + abs(tvec[1] - tvec_env[1]) < 0.005):
+        return 1
+    else:
+        return 0
+
+
+def FromQtoVec(qBeg, qEnd):
+    '''функция для нахождения вектора перемещения и матрицы перехода от одной конфигурации к другой'''
+
+    tvec = [0, 0, 0]
+    end_rmatx = [[], [], []]
+    beg_rmatx = [[], [], []]
+
+    for i in range(3):
+        tvec[i] = ((robot.fkine(qEnd).A)[i][3] - (robot.fkine(qBeg).A)[i][3])
+        beg_rmatx[i] = list((robot.fkine(qBeg).A)[i][:3])
+        end_rmatx[i] = list((robot.fkine(qEnd).A)[i][:3])
+
+    tvec = np.transpose(np.matmul(np.linalg.inv(np.array(beg_rmatx)), np.array(tvec).transpose()))
+
+    trans_mtx = np.linalg.inv(np.array(end_rmatx)).dot(np.array(beg_rmatx))
+    return tvec, trans_mtx
+
+
+def jtrajFromCurToGiven(qFinish):
+    sol = rtb.tools.trajectory.jtraj(robot.q, qFinish, 50)
+    return sol
+
+def ctrajFromCurToGiven(qFinish, time = 4):
+    step = 50
+
+    traj = rtb.tools.trajectory.ctraj(robot.fkine(robot.q), robot.fkine(qFinish), step)
+    cartesian_sol = robot.ikine_LM(traj)
+    
+    
+    velocity_array = np.ndarray(shape=(step - 1, 6))  # Создание массива для хранения скоростей в осях
+    for i in range(step - 1):  # вычисление обобщенных скоростей в осях для каждой точки траектории
+        for k in range(6):
+            velocity_array[i][k] = (cartesian_sol.q[i + 1][k] - cartesian_sol.q[i][k]) / float(time/step)
+
+    traj = rtb.tools.trajectory.Trajectory(name = "ctraj", s = cartesian_sol.q[1:], sd = velocity_array, t = time)
+    return traj
